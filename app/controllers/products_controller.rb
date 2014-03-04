5f1813b3bcbe0f0adee24b3615fbddd17f0be96c
arovillard <arovillard@gmail.com>
2014-03-04 17:09:55 -0500

    Show page and index page updaed and working

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,11 @@
 class ProductsController < ApplicationController
   before_filter :ensure_logged_in, :except => [:index,:show]
+  before_filter :find_product, :only => [:show, :edit, :update, :destroy]
   def index
     @products = Product.all
   end
 
   def show
-    @product = Product.find([:id])
   end
 
   def new
@@ -13,7 +13,7 @@ class ProductsController < ApplicationController
   end
 
   def create
-    @product = User.product.new(product_params)
+    @product = current_user.products.build(product_params)
     if @product.save
       redirect_to product_path(@product), notice: 'breakpoint created!'
     else
@@ -22,23 +22,24 @@ class ProductsController < ApplicationController
   end
 
   def edit
-    @product = Product.find([:id])
   end
 
   def update
-    @product = Product.find([:id])
     if @product.update_attributes(product_params)
       redirect_to products_path(@product)
     end
   end
 
   def destroy
-    @product = Product.find([:id])
     @product.destroy
     redirect_to products_path
   end
 
   private
+
+  def find_product
+    @product = Product.find(params[:id])
+  end
 
   def product_params
     params.require(:product).permit(:lender_id, :title, :description, :price)
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,12 @@ class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
   validates_confirmation_of :password
-  validates_presence_of :password, :on => :create
+  validates_presence_of :password, on: :create
   validates_presence_of :email
   validates_uniqueness_of :email
 
-  has_many :products
-  has_many :products, :through => :rentals
+  has_many :products, foreign_key: "lender_id"
+  has_many :rentals
+  # has_many :borrowed_products, through: :rentals, source: :user
 
 end
