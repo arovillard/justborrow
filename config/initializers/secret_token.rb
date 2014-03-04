2d97a2a4bc733b80f8bd919f45002882924c6422
arovillard <arovillard@gmail.com>
2014-03-03 21:27:00 -0500

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Justborrowit::Application.config.secret_key_base = '7d78d1ed49dceeefd4ba871d6b0a414f6a80b0d9f9fed4a177d58e7ace2e325a7f0b231aa37f7d541efc370c694c04d29e97f21d14eba2edc8fbb01c24c0323c'
