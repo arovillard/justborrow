a6985ab310f4568a4f3c5e3e07450abc64e0b786
hephalump <jason@sosnowskisearch.com>
2014-03-06 11:14:12 -0500

    Static pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Justborrowit::Application.routes.draw do
   get "logout" => "sessions#destroy", :as => "logout"
   get "login" => "sessions#new", :as => "login"
   get "signup" => "users#new", :as => "signup"
+
   resources :sessions, :only => [:new, :create, :destroy]
   resources :users
   resources :products do
