337cd38208f37d7a03b47b217155657db60e6782
hephalump <jason@sosnowskisearch.com>
2014-03-06 18:57:38 -0500

    Adding static pages

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,11 @@ Justborrowit::Application.routes.draw do
    resources :categories
   end
 
+  get 'help' => 'pages#help'
+  get 'terms' => 'pages#terms'
+  get 'contact' => 'pages#contact'
+  get 'affiliates' => 'pages#affiliates'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
